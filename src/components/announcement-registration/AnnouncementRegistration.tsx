d58927e62631a25260fd4698a041d658959236a8
Álvaro Rodrigues <alvarordgs98@gmail.com>
2024-11-17 14:55:24 -0300

    enhancing announcement register page

--- src/components/announcement-registration/AnnouncementRegistration.tsx
+++ src/components/announcement-registration/AnnouncementRegistration.tsx
@@ -34,24 +34,19 @@ export default function AnnouncementRegistration() {
 
   return (
     <>
-      <div className="relative z-10">
-        <Header />
-      </div>
-
-      {/* Adicionando espaçamento no topo dos cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 w-full min-h-screen pt-28">
-        {/* Seção Adoção */}
+      <Header />
+      <div className="grid grid-cols-1 md:grid-cols-3 w-full min-h-screen">
         <div
-          className="relative group bg-cover bg-center flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
+          className="relative group bg-cover bg-center bg-[url('/img/animal-adocao.jpg')] flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
           onMouseEnter={() => setHoverSection('adoção')}
           onMouseLeave={() => setHoverSection(null)}
           onClick={() => handleClick('adoção')}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center transition-all duration-300 transform group-hover:-translate-y-2">
-            <span>Cadastrar Animal para Adoção</span>
+          <div className="absolute inset-0 bg-black bg-opacity-70 hover:bg-opacity-0 hover:text-transparent flex items-center justify-center transition-all duration-300">
+            <span className='text-2xl'>Cadastrar Animal para Adoção</span>
           </div>
           {hoverSection === 'adoção' && (
-            <div className="absolute bottom-12 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100 transform group-hover:translate-y-4">
+            <div className="absolute bottom-0 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100">
               <h3 className="font-semibold">Recomendações para Adoção:</h3>
               <ul>
                 {recommendations.adoção.map((rec, idx) => (
@@ -61,19 +56,17 @@ export default function AnnouncementRegistration() {
             </div>
           )}
         </div>
-
-        {/* Seção Desaparecido */}
         <div
-          className="relative group bg-cover bg-center flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
+          className="relative group bg-cover bg-center bg-[url('/img/animal-perdido.avif')] flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
           onMouseEnter={() => setHoverSection('desaparecido')}
           onMouseLeave={() => setHoverSection(null)}
           onClick={() => handleClick('desaparecido')}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center transition-all duration-300 transform group-hover:-translate-y-2">
-            <span>Cadastrar Animal Desaparecido</span>
+          <div className="absolute inset-0 bg-black bg-opacity-70 hover:bg-opacity-0 hover:text-transparent flex items-center justify-center transition-all duration-300">
+            <span className='text-2xl'>Cadastrar Animal Desaparecido</span>
           </div>
           {hoverSection === 'desaparecido' && (
-            <div className="absolute bottom-12 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100 transform group-hover:translate-y-4">
+            <div className="absolute bottom-0 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100">
               <h3 className="font-semibold">
                 Recomendações para Animal Desaparecido:
               </h3>
@@ -85,18 +78,16 @@ export default function AnnouncementRegistration() {
             </div>
           )}
         </div>
-
-        {/* Seção Encontrado */}
         <div
-          className="relative group bg-cover bg-center flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
+          className="relative group bg-cover bg-center bg-[url('/img/animal-encontrado.jpg')] flex items-center justify-center text-white text-xl font-bold cursor-pointer transition-all duration-300"
           onMouseEnter={() => setHoverSection('encontrado')}
           onMouseLeave={() => setHoverSection(null)}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center transition-all duration-300 transform group-hover:-translate-y-2">
-            <span>Cadastrar Animal Encontrado</span>
+          <div className="absolute inset-0 bg-black bg-opacity-70 hover:bg-opacity-0 hover:text-transparent flex items-center justify-center transition-all duration-300">
+            <span className='text-2xl'>Cadastrar Animal Encontrado</span>
           </div>
           {hoverSection === 'encontrado' && (
-            <div className="absolute bottom-12 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100 transform group-hover:translate-y-4">
+            <div className="absolute bottom-0 left-0 right-0 bg-white p-4 text-sm text-gray-700 shadow-lg transition-all duration-300 opacity-100">
               <h3 className="font-semibold">
                 Recomendações para Animal Encontrado:
               </h3>
